8ca64bfec5fe3749c212dcc29bf63a37f44ea907
Prasad <shprasad@paypal.com>
2014-12-11 12:42:08 +0000

    Signup

---
+++ public/js/myJs.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+		$("#signup").click(function(){
+			window.open('https://www.stage2md041.qa.paypal.com/webapps/merchantboarding/webflow/unifiedflow?execution=e1s1');
+			});
+		});
+
